d6df35dc7c5d7a4708e6909c1b4ee2ec113529f8
Seon-Wook Park <seon.wook@swook.net>
2014-01-28 15:10:46 +0100

    js: Add comment for min versions. Add min.js

--- socialcounts.js
+++ socialcounts.js
@@ -18,6 +18,8 @@
    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+/*! SocialCountsJS / @author: Seon-Wook Park / @license: LGPLv3 */
+
 (function(){
 	var prot_chk   = /^[htfps]+:\/\/.*/,
 
