43bd9f2c080a24d898a233937fba0f8a1edeb40d
Seon-Wook Park <seon.wook@swook.net>
2014-06-26 17:57:18 +0900

    widgets: Implement share text and click

--- widgets/js/common.js
+++ widgets/js/common.js
@@ -54,43 +54,56 @@
     googleplus: {
       name: "Google+",
       cdesc: "+1s", cdescs: "+1",
+      stxt: "Share",
       surl: "https://plus.google.com/share?url="
     },
     facebook: {
       name: "Facebook",
       cdesc: "likes", cdescs: "like",
+      stxt: "Share",
       surl: "http://www.facebook.com/sharer/sharer.php?u="
     },
     twitter: {
       name: "Twitter",
       cdesc: "tweets", cdescs: "tweet",
+      stxt: "Tweet",
       surl: "http://twitter.com/share?url="
     },
     linkedin: {
       name: "LinkedIn",
       cdesc: "shares", cdescs: "share",
+      stxt: "Share",
       surl: "http://www.linkedin.com/shareArticle?url="
     },
     reddit: {
       name: "Reddit",
       cdesc: "points", cdescs: "point",
+      stxt: "Share",
       surl: "http://www.reddit.com/submit?url="
     },
     stumbleupon: {
       name: "StumbleUpon",
       cdesc: "stumbles", cdescs: "stumble",
+      stxt: "Stumble",
       surl: "https://www.stumbleupon.com/submit?url="
     },
     delicious: {
       name: "Delicious",
       cdesc: "bookmarks", cdescs: "bookmark",
+      stxt: "Save",
       surl: "https://delicious.com/save?url="
     },
     pinterest: {
       name: "Pinterest",
       cdesc: "pins", cdescs: "pin",
+      stxt: "Pin",
       surl: "http://pinterest.com/pin/create/button/?url="
     }
+  };
+
+  // Interaction handlers
+  var onclickShare = function(svc) {
+    window.open(svc.surl + encodeURIComponent(url));
   };
 
   // Store DOMElement references per social network
@@ -100,7 +113,8 @@
         el,
         tmpl = _.template("<img class=\"icon\" src=\"img/<%= shortName %>-64.png\">" +
                           "<span class=\"counts\">&#x22EF;</span>" +
-                          "<span class=\"clabel\"></span>");
+                          "<span class=\"clabel\"></span>" +
+                          "<span class=\"sharelbl\"></span>");
     _.each(cfg.svcs, function(sn) {
       if (!(sn in sn_cfg)) return;
       sn_cfg[sn].shortName = sn;
@@ -116,24 +130,39 @@
       svc = {
         el: el,
         c:  el.getElementsByClassName("counts")[0],
-        cl: el.getElementsByClassName("clabel")[0]
+        cl: el.getElementsByClassName("clabel")[0],
+        sl: el.getElementsByClassName("sharelbl")[0]
       };
+      _.extend(svc, sn_cfg[sn]);
+
+      // Set share text
+      svc.sl.innerHTML = svc.stxt;
+
+      if ("widgetType" in window && window.widgetType === "Button") {
+        // Set hover handler for placing button iframe
+      } else {
+        // Set click handler for sharing
+        el.onclick = onclickShare.bind(el, svc);
+      }
 
       // Add to list
-      _.extend(svc, sn_cfg[sn]);
       svcs.push(svc);
     });
   };
 
   // updateCounts queries SocialCountsJS and updates on-screen counts
   var updateCounts = function() {
+    var c;
     socialcounts(url, function(data) {
       // Apply formatting to received numbers
       data = socialcounts.fmt[cfg.fmt](data);
 
       // Show numbers on page
       _.each(svcs, function(svc) {
-        svc.c.innerHTML = data.counts[svc.shortName];
+        c = data.counts[svc.shortName];
+        svc.c.innerHTML = c;
+        if (c === 1) svc.cl.innerHTML = svc.cdescs;
+        else         svc.cl.innerHTML = svc.cdesc;
       });
     });
   };
