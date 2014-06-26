561d9a84d3df19a1631d2e3fbbaaf20bca9ef2ff
Seon-Wook Park <seon.wook@swook.net>
2014-06-26 20:49:05 +0900

    widgets: Add buttons and widget 6 (same as 5 but with buttons on hover)

--- widgets/js/common.js
+++ widgets/js/common.js
@@ -55,37 +55,42 @@
       name: "Google+",
       cdesc: "+1s", cdescs: "+1",
       stxt: "Share",
-      surl: "https://plus.google.com/share?url="
+      surl: "https://plus.google.com/share?url=",
+      burl: "https://apis.google.com/u/0/_/+1/fastbutton?annotation=none&url=<%= url %>"
     },
     facebook: {
       name: "Facebook",
       cdesc: "likes", cdescs: "like",
       stxt: "Share",
-      surl: "http://www.facebook.com/sharer/sharer.php?u="
+      surl: "https://www.facebook.com/sharer/sharer.php?u=",
+      burl: "https://www.facebook.com/plugins/like.php?href=<%= url %>&amp;width&amp;layout=button&amp;action=like&amp;show_faces=false&amp;share=false&amp;height=35"
     },
     twitter: {
       name: "Twitter",
       cdesc: "tweets", cdescs: "tweet",
       stxt: "Tweet",
-      surl: "http://twitter.com/share?url="
+      surl: "https://twitter.com/share?url=",
+      burl: "https://platform.twitter.com/widgets/tweet_button.html#count=none&dnt=true&id=twitter-widget-16&lang=en&size=m&url=<%= url %>"
     },
     linkedin: {
       name: "LinkedIn",
       cdesc: "shares", cdescs: "share",
       stxt: "Share",
-      surl: "http://www.linkedin.com/shareArticle?url="
+      surl: "https://www.linkedin.com/shareArticle?url=",
+      bimg: "img/btn_linkedin.png"
     },
     reddit: {
       name: "Reddit",
       cdesc: "points", cdescs: "point",
       stxt: "Share",
-      surl: "http://www.reddit.com/submit?url="
+      surl: "https://www.reddit.com/submit?url="
     },
     stumbleupon: {
       name: "StumbleUpon",
       cdesc: "stumbles", cdescs: "stumble",
       stxt: "Stumble",
-      surl: "https://www.stumbleupon.com/submit?url="
+      surl: "https://www.stumbleupon.com/submit?url=",
+      burl: "http://badge.stumbleupon.com/badge/embed/3/?url=<%= url %>"
     },
     delicious: {
       name: "Delicious",
@@ -97,13 +102,27 @@
       name: "Pinterest",
       cdesc: "pins", cdescs: "pin",
       stxt: "Pin",
-      surl: "http://pinterest.com/pin/create/button/?url="
+      surl: "https://pinterest.com/pin/create/button/?url=",
+      bimg: "https://assets.pinterest.com/images/pidgets/pinit_fg_en_rect_gray_20.png"
     }
   };
 
   // Interaction handlers
   var onclickShare = function(svc) {
     window.open(svc.surl + encodeURIComponent(url));
+  };
+
+  var onhoverButton = function(svc) {
+    if (svc.btn.innerHTML !== "" || (!svc.bimg && !svc.burl)) return;
+
+    if (svc.burl) {
+      svc.btn.innerHTML = "<iframe src=\"" +
+                          _.template(svc.burl, {url: encodeURIComponent(url)}) +
+                          "\"></iframe>";
+    } else if (svc.bimg) {
+      svc.btn.innerHTML = "<img src=\"" + svc.bimg + "\">";
+      svc.btn.getElementsByTagName("img")[0].onclick = onclickShare.bind(this, svc);
+    }
   };
 
   // Store DOMElement references per social network.
@@ -121,7 +140,8 @@
                           ".png\">" +
                           "<span class=\"counts\">&#x22EF;</span>" +
                           "<span class=\"clabel\"></span>" +
-                          "<span class=\"sharelbl\"></span>");
+                          "<span class=\"sharelbl\"></span>" +
+                          "<span class=\"button\"></span>");
     _.each(cfg.svcs, function(sn) {
       if (!(sn in sn_cfg)) return;
       sn_cfg[sn].shortName = sn;
@@ -135,10 +155,11 @@
 
       // Set references to sub elements
       svc = {
-        el: el,
-        c:  el.getElementsByClassName("counts")[0],
-        cl: el.getElementsByClassName("clabel")[0],
-        sl: el.getElementsByClassName("sharelbl")[0]
+        el:  el,
+        c:   el.getElementsByClassName("counts")[0],
+        cl:  el.getElementsByClassName("clabel")[0],
+        sl:  el.getElementsByClassName("sharelbl")[0],
+        btn: el.getElementsByClassName("button")[0]
       };
       _.extend(svc, sn_cfg[sn]);
 
@@ -148,6 +169,7 @@
 
       if ("widgetType" in window && window.widgetType === "Button") {
         // Set hover handler for placing button iframe
+        el.onmouseover = onhoverButton.bind(el, svc);
       } else {
         // Set click handler for sharing
         el.onclick = onclickShare.bind(el, svc);
